5bd237780ccd6dc2c0256790e38b2572620b065e
Elena Karpenko <lena.a.karpenko@gmail.com>
2018-04-16 20:35:32 +0300

    Initial Commit

---
+++ FitnessLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitnessLife
+//
+//  Created by Karpenko Elena on 4/16/18.
+//  Copyright © 2018 Karpenko Elena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
